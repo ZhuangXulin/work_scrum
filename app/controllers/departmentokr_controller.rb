36e87cf41967ceeff59a6923c000e493b36df91e
zhuangxulin2003 <zhuangxulin2003@gmail.com>
2016-11-23 17:15:16 +0800

    add department_okrs

--- app/controllers/departmentokr_controller.rb
+++ app/controllers/departmentokr_controller.rb
@@ -32,6 +32,19 @@ class DepartmentokrController < ApplicationController
 			#部门名称
 			@department_name = Department.get_department_name(@department_id).department_name
 		end
+		#计算部门总分
+		@total_score = 0
+		@total_proportion = 0
+		if !@department_okrs.nil?
+			@department_okrs.each {|department_okr| 
+			if !department_okr.okr_score.nil?
+				@total_score += department_okr.okr_score
+			end
+			if !department_okr.okr_proportion .nil?
+				@total_proportion += department_okr.okr_proportion 
+			end
+			}
+		end
 	end
 
 	def new
@@ -55,20 +68,45 @@ class DepartmentokrController < ApplicationController
 
 	def edit
 		@department_okr = Departmentokr.find(params[:id])
+		#日期列表
 		@date_list = BasicDate.get_date_list
+		#完成状态列表
+		@okr_stats = OkrStat.get_okr_data_list
+		#难度系数列表
+		@degrees = DifficultyDegree.get_okr_degree_list
     	@action = :update
   	end
 
 	def update
-  		@department_okr = Departmentokr.show(params[:id])
+  		@department_okr = Departmentokr.find(params[:id])
+
   		respond_to do |format|
-    		if @department_okr.update_attributes(:okr_name => department_okr_params["okr_name"],
+  			#系统管理员能够对OKR进行评定，部门管理员只能够修改自己的OKR内容
+  			if @current_user_role == "admin"
+  				degree_number = DifficultyDegree.get_okr_degree_number(department_okr_params[:okr_degree_of_difficulty])
+  				okr_number = OkrStat.get_okr_number(department_okr_params[:okr_stats])
+  				#计算得分
+  				score = (okr_number * degree_number * department_okr_params[:okr_proportion].to_f).round(3)
+  				result = @department_okr.update_attributes(:okr_name => department_okr_params["okr_name"],
+                                      :okr_date => department_okr_params["okr_date"],:okr_proportion => department_okr_params[:okr_proportion],:okr_stats => department_okr_params[:okr_stats],
+                                      :okr_score => score,:description => department_okr_params[:description],:assessment_time => Time.new.strftime("%Y-%m-%d %H:%M:%S"),
+                                      :assessment_person => current_user.id,:okr_degree_of_difficulty => department_okr_params[:okr_degree_of_difficulty]
+                                      )
+  			elsif @current_user_role == "manager"
+  				if @department_okr.okr_score.nil?
+  					result = @department_okr.update_attributes(:okr_name => department_okr_params["okr_name"],
                                       :okr_date => department_okr_params["okr_date"]
                                       )
-	    		
-          		format.html { redirect_to department_okr_url, notice: 'Department OKR was successfully updated.' }
+  				else
+  					result = false
+  				end
+  				
+  			end
+
+    		if result
+          		format.html { redirect_to departmentokr_url, notice: 'Department OKR was successfully updated.' }
     		else
-      			format.html { render action: "edit" ,notice: 'Department OKR was unsuccessfully updated.'}
+      			format.html { redirect_to departmentokr_url ,notice: 'Department OKR can not update after assessment.'}
     		end
     	end
   	end
@@ -82,14 +120,13 @@ class DepartmentokrController < ApplicationController
 	end
 
 	def show
-    	@department_okr = Departmentokr.find(params[:id])
-    	@department_name = Department.get_department_name(@department_okr.department_id).department_name
+    	@department_okr = Departmentokr.get_department_okr_info(params[:id])
   	end
 
 
 	private
   	def department_okr_params
-    	params.require(:departmentokr).permit(:id,:department_id,:okr_name,:okr_date)
+    	params.require(:departmentokr).permit(:id,:department_id,:okr_name,:okr_date,:okr_proportion,:okr_score,:okr_stats,:okr_degree_of_difficulty,:description)
   	end
 
  end
--- app/models/departmentokr.rb
+++ app/models/departmentokr.rb
@@ -8,6 +8,7 @@ class Departmentokr < ActiveRecord::Base
 
 	self.per_page = 15
 
+	#获取OKR列表
 	def self.get_department_okrs(department_id,okr_date,page)
 		if department_id.nil? || department_id == 0
 			#Departmentokr.joins(:Department).where(:okr_date => okr_date).paginate(:page => page, :per_page => per_page)
@@ -15,6 +16,15 @@ class Departmentokr < ActiveRecord::Base
 		else
 			Departmentokr.paginate_by_sql("SELECT 'departmentokrs'.*,'departments'.'department_name' FROM 'departmentokrs' left JOIN 'departments' ON 'departments'.'id' = 'departmentokrs'.'department_id' WHERE 'departmentokrs'.'okr_date' = '#{okr_date}' AND 'departmentokrs'.'department_id' = #{department_id} " ,:page => page, :per_page => per_page)
 		end
+	end
+
+	def self.get_department_okr_info(okr_id)
+		sql = "select departmentokrs.*,(select email from users where id = departmentokrs.create_user_id ) as create_user_email,
+			(select email from users where id = departmentokrs.assessment_person) as assessment_person_email,
+			(select stats_name from okr_stats where id = departmentokrs.okr_stats) as okr_stats_name,
+			(select department_name from departments where id = departmentokrs.department_id) as department_name,
+			(select degree_name from difficulty_degrees where id = departmentokrs.okr_degree_of_difficulty) as okr_degree_of_difficulty_name from departmentokrs where departmentokrs.id = #{okr_id}"
+		Departmentokr.find_by_sql(sql).first
 	end
 
 end
