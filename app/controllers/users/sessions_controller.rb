a569f3a84ab52ecb1f062ba7054138f7b4886479
zhuangxulin2003 <zhuangxulin2003@gmail.com>
2016-12-02 09:27:58 +0800

    增加验证码

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   skip_before_filter :require_no_authentication, :only => [:create]
   #before_filter :configure_sign_in_params, only: [:create]
   #验证码
-  #include SimpleCaptcha::ControllerHelpers
+  include SimpleCaptcha::ControllerHelpers
 
   # GET /resource/sign_in
   # def new
@@ -10,15 +10,15 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  #def create
-  #  if simple_captcha_valid?
-  #    super
-  #  else
-  #    flash[:alert] = t("simple_captcha.message.error")
-  #    self.resource = resource_class.new(sign_in_params)
-  #    respond_with_navigational(resource) { render :new }
-  #  end 
-  #end
+  def create
+    if simple_captcha_valid?
+      super
+    else
+      flash[:alert] = t("simple_captcha.message.error")
+      self.resource = resource_class.new(sign_in_params)
+      respond_with_navigational(resource) { render :new }
+    end 
+  end
 
   # DELETE /resource/sign_out
   # def destroy
